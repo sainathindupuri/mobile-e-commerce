f4ff251643aaf27830adc68258b12628b72ac875
Indupuri.Sainath <indupuri.sainath@cognizant.com>
2019-09-10 00:09:31 +0530

    styling product cards

--- src/components/Product.js
+++ src/components/Product.js
@@ -21,13 +21,13 @@ export default class Product extends Component {
                         </button>
                     </div>
                     <div className="card-footer d-flex justify-content-between">
-                    <p className="align-self-center mb-0">
-                    {title}
-                    </p>
-                    <h5 className="text-blue font-italic mb-0">
-                    <span className="mr-1">$</span>
-                    {price}
-                    </h5>
+                        <p className="align-self-center mb-0">
+                            {title}
+                        </p>
+                        <h5 className="text-blue font-italic mb-0">
+                            <span className="mr-1">$</span>
+                            {price}
+                        </h5>
                     </div>
                 </div>
             </ProductWrapper>
@@ -37,4 +37,53 @@ export default class Product extends Component {
 
 
 const ProductWrapper = styled.div`
+.card{
+    border-color: transparent;
+    transition: all 1s linear;  
+
+}
+.card-footer{
+    boder-top:transparent;
+    background: transparent;
+    transition: all 1s linear;
+}
+&:hover{
+    .card{
+        border:0.04rem solid rgba(0,0,0,0.2);
+        box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.2);
+    }
+    .card-footer{
+        background: rgba(247,247,247);
+    }    
+}
+.img-container{
+    position:relative;
+    overflow:hidden;
+}
+.img-container:hover .card-img-top{
+    transform:scale(1.2);
+}
+.card-img-top{
+    transition: all 1s linear;
+}
+.cart-btn{
+    position:absolute;
+    bottom:0;
+    right:0;
+    padding:0.2rem  0.4rem;
+    background:var(--lightBlue);
+    border:none;
+    color:var(--mainWhite);
+    font-size:1.4rem;
+    border-radius: 0.5rem 0 0 0;
+    transform:translate(100%,100%);
+    transition: all 1s linear;
+    }
+    .img-container:hover .cart-btn{
+        transform:translate(0,0);
+    }
+    .cart-btn:hover{
+        color:var(--mainBlue);
+        cursor:pointer;
+    }
 `
